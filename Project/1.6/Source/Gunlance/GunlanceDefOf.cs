a16de0fc4f0f64b725099d6dd5f068db30e71e1a
solaris <solaris0115@naver.com>
2025-11-16 00:38:41 +0900

    건랜스 이펙트 관련 코드 수정

--- Project/1.6/Source/Gunlance/GunlanceDefOf.cs
+++ Project/1.6/Source/Gunlance/GunlanceDefOf.cs
@@ -13,10 +13,10 @@ namespace NewRatkin
     [DefOf]
     public static class RatkinSoundDefOf
     {
-        public static SoundDef RK_Charge;
-        public static SoundDef RK_Fire;
-        public static SoundDef RK_OverHeat;
-        public static SoundDef RK_Reload;
-        public static SoundDef RK_WyvernFire;
+        // public static SoundDef RK_Charge;
+        // public static SoundDef RK_Fire;
+        // public static SoundDef RK_OverHeat;
+        // public static SoundDef RK_Reload;
+        // public static SoundDef RK_WyvernFire;
     }
 }
