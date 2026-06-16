7c4a589c5062fc818d59966b307fcd80602f05d0
solaris <solaris0115@naver.com>
2026-06-17 03:10:25 +0900

    Apply gene draw scale during render setup

--- Project/1.6/Source/Gene_DrawScale.cs
+++ Project/1.6/Source/Gene_DrawScale.cs
@@ -7,13 +7,11 @@ namespace NewRatkin
 	public class GeneDrawScaleDef : GeneDef
 	{
 		public float drawScale = 1f;
-		public int reapplyIntervalTicks = 60;
 	}
 
 	public class Gene_DrawScale : Gene
 	{
 		private const float MinDrawScale = 0.01f;
-		private const int DefaultReapplyIntervalTicks = 60;
 
 		private GeneDrawScaleDef DrawScaleDef
 		{
@@ -23,21 +21,12 @@ namespace NewRatkin
 			}
 		}
 
-		private float DrawScale
+		internal float DrawScale
 		{
 			get
 			{
 				GeneDrawScaleDef drawScaleDef = DrawScaleDef;
 				return Mathf.Max(MinDrawScale, drawScaleDef != null ? drawScaleDef.drawScale : 1f);
-			}
-		}
-
-		private int ReapplyIntervalTicks
-		{
-			get
-			{
-				GeneDrawScaleDef drawScaleDef = DrawScaleDef;
-				return Mathf.Max(1, drawScaleDef != null ? drawScaleDef.reapplyIntervalTicks : DefaultReapplyIntervalTicks);
 			}
 		}
 
@@ -56,15 +45,6 @@ namespace NewRatkin
 			ApplyScaleToPawn(pawn, DrawScaleForPawn(pawn, this), rebuildTree: true, dirtyCaches: true);
 		}
 
-		public override void TickInterval(int delta)
-		{
-			base.TickInterval(delta);
-			if (pawn != null && pawn.IsHashIntervalTick(ReapplyIntervalTicks, delta))
-			{
-				ApplyScaleToPawn(pawn, DrawScale, rebuildTree: false, dirtyCaches: false);
-			}
-		}
-
 		private static float DrawScaleForPawn(Pawn pawn, Gene ignoredGene)
 		{
 			if (pawn == null || pawn.genes == null)
@@ -80,6 +60,15 @@ namespace NewRatkin
 				}
 			}
 			return 1f;
+		}
+
+		internal static void ApplyScaleToRenderTree(PawnRenderTree renderTree, float scale)
+		{
+			PawnRenderNode rootNode = renderTree != null ? renderTree.rootNode : null;
+			if (rootNode != null && !Mathf.Approximately(rootNode.debugScale, scale))
+			{
+				rootNode.debugScale = scale;
+			}
 		}
 
 		private static void ApplyScaleToPawn(Pawn pawn, float scale, bool rebuildTree, bool dirtyCaches)
@@ -102,18 +91,9 @@ namespace NewRatkin
 			}
 			renderer.EnsureGraphicsInitialized();
 
-			PawnRenderNode rootNode = renderTree.rootNode;
-			if (rootNode == null)
-			{
-				return;
-			}
-
-			bool scaleChanged = !Mathf.Approximately(rootNode.debugScale, scale);
-			if (scaleChanged)
-			{
-				rootNode.debugScale = scale;
-			}
-			if (dirtyCaches && (scaleChanged || rebuildTree))
+			float scaleBefore = renderTree.rootNode != null ? renderTree.rootNode.debugScale : 1f;
+			ApplyScaleToRenderTree(renderTree, scale);
+			if (dirtyCaches && (rebuildTree || !Mathf.Approximately(scaleBefore, scale)))
 			{
 				DirtyVisualCaches(pawn);
 			}
@@ -124,6 +104,24 @@ namespace NewRatkin
 			SilhouetteUtility.NotifyGraphicDirty(pawn);
 			PortraitsCache.SetDirty(pawn);
 			GlobalTextureAtlasManager.TryMarkPawnFrameSetDirty(pawn);
+		}
+	}
+
+	public class PawnRenderNodeWorker_DrawScale : PawnRenderNodeWorker
+	{
+		public override bool ShouldListOnGraph(PawnRenderNode node, PawnDrawParms parms)
+		{
+			return false;
+		}
+
+		public override bool CanDrawNow(PawnRenderNode node, PawnDrawParms parms)
+		{
+			Gene_DrawScale drawScaleGene = node.gene as Gene_DrawScale;
+			if (drawScaleGene != null && drawScaleGene.Active)
+			{
+				Gene_DrawScale.ApplyScaleToRenderTree(node.tree, drawScaleGene.DrawScale);
+			}
+			return false;
 		}
 	}
 }
